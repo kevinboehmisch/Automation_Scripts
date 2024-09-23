2280e983b6ad6c877101cbb61cf065bcda41826e
kevinboehmisch <150022344+kevinboehmisch@users.noreply.github.com>
2024-09-23 20:19:09 +0200

    Create simple_mail.py

---
+++ E-Mail_Scripts/simple_mail.py
@@ -0,0 +1,26 @@
+import os
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+email_sender = 'mrwasgehtsiedasan@gmail.com'
+email_password = 'password'
+email_receiver = 'receiver@receiver.com'
+
+subject = 'test'
+body = 'das ist ein test'
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['Subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
+    
+    
+
